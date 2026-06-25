79f2890c1709539932aecd8b438d660ce6fbd39a
Raj_Gaurav2691 <rajgaura327@gmail.com>
2026-06-26 04:21:46 +0530

    JUnitBasicTesting Excercise Completed

---
+++ DN - Java FSE Mandatory hands-on_Week1/TDD using JUnit5 and Mockito/JUnitBasicTesting/src/test/java/Exercise1_HelloTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class Exercise1_HelloTest {
+
+    @Test
+    public void test() {
+        System.out.println("JUnit is working!");
+    }
+}
---
+++ DN - Java FSE Mandatory hands-on_Week1/TDD using JUnit5 and Mockito/JUnitBasicTesting/src/test/java/Exercise4_CalculatorTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+        System.out.println("Setup");
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Teardown");
+    }
+
+    @Test
+    public void testAdd() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+}
